557c1a491278c68463cb43fbaa21ab7aac53c4f4
jerryWang <luckboystudent@gmail.com>
2018-06-14 16:37:46 +0800

    20180614

--- swimapp/model/list_group.php
+++ swimapp/model/list_group.php
@@ -17,10 +17,13 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM `Team` WHERE `U_num` = (SELECT `U_num` FROM `USER` WHERE `U_mail`='".$token."')";
 $result = $conn->query($sql);
 $group='{"group":[';
+$groupPic='{"name":'.$token.',"children":[';
 $count=@$result->num_rows;
-
+$myfile =fopen("../json/".$token.".json", "wb");
+fwrite($myfile, $groupPic);
 if (@$result->num_rows > 0) {
 	echo $group; 
+    // echo $groupPic;
 	while($row = $result->fetch_assoc()) {
         $arr=array('T_num' => $row['T_num'], 'U_num' => $row['U_num'], 'T_name' => $row['T_name'],'T_info' => $row['T_info']);
         if($count!=1){
@@ -28,10 +31,21 @@ if (@$result->num_rows > 0) {
         }else{
 			echo json_encode($arr);
         }
+
+        
+        $size=3938;
+        $arr2=array('name' =>$row['T_num'],'size'=>3398);
+        if($count!=1){
+            // echo json_encode($arr2).",";
+            fwrite($myfile, json_encode($arr2).",");
+        }else{
+            // echo json_encode($arr2);
+            fwrite($myfile, json_encode($arr2));
+        }
 		$count--;
     }
     echo "]}";
-
+    fwrite($myfile, "]}");
 } else {
     echo "false";
 }
