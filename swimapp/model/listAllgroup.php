fb73309138f611ffee18b565ccf92937024be29d
jerryWang <luckboystudent@gmail.com>
2018-06-21 00:17:22 +0800

    20180621

--- swimapp/model/listAllgroup.php
+++ swimapp/model/listAllgroup.php
@@ -18,7 +18,7 @@ if(isset($token)){
 	$count=@$result->num_rows;
 
 	if(@$result->num_rows==0){
-		$group='{"group":[';
+		$group='{"name":"root" ,"children":[';
 		$sql2="SELECT * FROM `Team` WHERE 1";
 		$res=$conn->query($sql2);
 		$count2=@$res->num_rows;
@@ -33,27 +33,50 @@ if(isset($token)){
 		        }
 				$count2--;
     		}
-    		echo "]}";
+    		echo "], \"size\":\"3398\"}";
 		}else{
 			echo "false";
 		}
 		
 	}else{
-
-		$group='{"group":[';
+		$myfile =fopen("../json/allGroupMember.json", "wb");
+		$group='{"name":"root" ,"children":[';
+		fwrite($myfile, $group);
 		$count=@$result->num_rows;
 		if (@$result->num_rows > 0) {
 			echo $group; 
 			while($row = $result->fetch_assoc()) {
-		        $arr=array('T_num' => $row['T_num'], 'U_num' => $row['U_num'], 'T_name' => $row['T_name'],'T_info' => $row['T_info']);
+				$children="[";
+				$sqlChildren="SELECT `U_mail`,`U_name` FROM `USER` WHERE `U_num` in (SELECT `U_num` FROM `Group_List` WHERE `T_num`=".$row['T_num'].")";
+				$resChildren=$conn->query($sqlChildren);
+				$childCount=0;
+				while($rowChildren = $resChildren->fetch_assoc()){
+
+					$arrChild=array('name' =>$rowChildren['U_name'], 'size'=>3398 );
+					$children=$children.json_encode($arrChild);
+					if(++$childCount!=$resChildren->num_rows)
+						$children=$children.",";
+				}
+				$children=$children."]";
+				// fwrite($myfile, $children);
+				$T_num=$row['T_num'];
+				$T_name= $row['T_name'];
+				$T_info=$row['T_info'];
+		        $arr=array('T_num' => $row['T_num'],'T_name' => $row['T_name'],'T_info' => $row['T_info'],'children'=> $children);
 		        if($count!=1){
-					echo json_encode($arr).",";
+		        	// fwrite($myfile, json_encode($arr));
+		        	fwrite($myfile, '{"T_num":'.$T_num.',"name":"'.$T_name.'","T_info":"'.$T_info.'","children":'.$children.',"size":"6000"},');
+		        	echo '{"T_num":'.$T_num.',"T_name":"'.$T_name.'","T_info":"'.$T_info.'","children":'.$children.',"size":"5000"},';
+					// echo json_encode($arr).",";
 		        }else{
-					echo json_encode($arr);
+					// echo json_encode($arr);
+					echo '{"T_num":'.$T_num.',"T_name":"'.$T_name.'","T_info":"'.$T_info.'","children":'.$children.'}';
+					fwrite($myfile, '{"T_num":'.$T_num.',"name":"'.$T_name.'","T_info":"'.$T_info.'","children":'.$children.',"size":"6000"}');
 		        }
 				$count--;
 		    }
-		    echo "]}";
+		    echo "], \"size\":\"3398\"}";
+		    fwrite($myfile, "], \"size\":\"3398\"}");
 
 		} else {
 		    echo "false";
